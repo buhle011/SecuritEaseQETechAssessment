a822dfb0ba44aac86e90687a7fd3feeaa9c13977
lungani delihlazo <banzi5380@gmail.com>
2025-03-12 09:46:34 +0200

    add changes

--- cucumber.js
+++ cucumber.js
@@ -1,9 +1,8 @@
 module.exports = {
   default: {
     require: ['stepdefinitions/*.js','accelarators/hooks.js'],
-    format:  ['progress', 'html:reports/cucumber-report.html'],
+    format: ['progress', 'json:reports/cucumber-report.json'], 
     parallel: 1,
     timeout: 60000,
-    //tags :'@test',
   },
 };
